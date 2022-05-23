99ad501287863745b6f75d8e27899233d3527705
ZhouYixun <291028775@qq.com>
2022-05-24 01:57:35 +0800

    预研解析输出2

---
+++ springboard_test.go
@@ -0,0 +1,26 @@
+package giDevice
+
+import (
+	"fmt"
+	"testing"
+)
+
+var springBoardSrv SpringBoard
+
+func setupSpringBoardSrv(t *testing.T) {
+	setupLockdownSrv(t)
+
+	var err error
+	if lockdownSrv, err = dev.lockdownService(); err != nil {
+		t.Fatal(err)
+	}
+
+	if springBoardSrv, err = lockdownSrv.SpringBoardService(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test_springBoard(t *testing.T) {
+	setupSpringBoardSrv(t)
+	fmt.Println(springBoardSrv.GetIconState())
+}
